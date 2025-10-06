5ef2408b2465dff3e4d28490187bdd9b33220666
Dominic Chen <cchendominic@gmail.com>
2025-10-06 16:15:19 -0400

    Modify button logic in index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 let homeScore = document.getElementById("home-score")
 let guestScore = document.getElementById("guest-score")
-let score = 0
+let score1 = 0
+let score2 = 0
 
-function incrOne() {
-    
+function addPoints(team, points) {
+    if (team === "home") {
+        score1 += points
+        homeScore.textContent = score1
+    } else {
+        score2 += points
+        guestScore.textContent = score2
+    }
 }
 
-function incrTwo() {
-    
-}
-
-function incrThree() {
-    
-}
