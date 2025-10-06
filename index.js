994dfbcfdb4d88063f46f53f5cb1636bd66514f1
Dominic Chen <cchendominic@gmail.com>
2025-10-06 15:54:34 -0400

    Create new files and populate index.html

---
+++ index.js
@@ -0,0 +1,15 @@
+let homeScore = document.getElementById("home-score")
+let guestScore = document.getElementById("guest-score")
+let score = 0
+
+function incrOne() {
+    
+}
+
+function incrTwo() {
+    
+}
+
+function incrThree() {
+    
+}
